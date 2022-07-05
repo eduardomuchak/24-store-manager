f72d3f2e7d6d2281c8c7abdc917d84417c32778a
Eduardo Muchak <eduardomuchak@gmail.com>
2022-07-05 20:25:05 +0000

    test: camada productController finalizada 100%

--- tests/unit/controllers/productController.test.js
+++ tests/unit/controllers/productController.test.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const { expect, use } = require('chai');
 const { ValidationError } = require('joi');
 const chaiAsPromised = require('chai-as-promised')
@@ -5,8 +6,7 @@ const sinon = require('sinon');
 const productsMock = require('../mocks/products.mock.js');
 const productService = require('../../../services/productService');
 const productController = require('../../../controllers/productController');
-
-require('express-async-errors');
+const NotFoundError = require('../../../errors/NotFoundError.js');
 
 use(chaiAsPromised);
 
@@ -49,5 +49,96 @@ describe('#Testa o productController', () => {
       expect(res.status.calledWith(200)).to.be.equal(true);
       expect(res.json.calledWith(productsMock[1])).to.be.equal(true);
     });
+  });
+
+  describe('#addProduct', () => {
+    it('deve retornar um erro ao tentar adicionar um produto com o corpo da requisição inválido', async () => {
+      const req = {};
+      const res = {};
+
+      req.body = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+
+      expect(productController.addProduct(req, res)).to.be.rejectedWith(ValidationError);
+    });
+
+    it('deve retornar o produto cadastrado ao tentar adicionar um produto com o corpo da requisição válido', async () => {
+      const req = {};
+      const res = {};
+
+      req.body = { name: "Armadura do Homem de Ferro" };
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+
+      sinon.stub(productService, 'addProduct').resolves({ id: 4, name: "Armadura do Homem de Ferro" });
+
+      await productController.addProduct(req, res);
+
+      expect(res.status.calledWith(201)).to.be.equal(true);
+      expect(res.json.calledWith({ id: 4, name: "Armadura do Homem de Ferro" })).to.be.equal(true);
+    });
+  });
+
+  describe('#editProduct', () => {
+    it('deve retornar um erro ao tentar editar um produto com o corpo da requisição inválido', async () => {
+      const req = {};
+      const res = {};
+
+      req.body = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+
+      expect(productController.editProduct(req, res)).to.be.rejectedWith(ValidationError);
+    });
+
+    it('deve retornar o produto editado ao tentar editar um produto com o corpo da requisição válido', async () => {
+      const req = {};
+      const res = {};
+      
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+      
+      req.params = { id: 2 };
+      req.body = { name: "Manopla do Infinito" };
+      
+      sinon.stub(productService, 'editProduct').resolves({id: 2, name: "Manopla do Infinito"});
+
+      await productController.editProduct(req, res);
+
+      expect(res.status.calledWith(200)).to.be.equal(true);
+      // expect(res.json.calledWith({ id: 2, name: "Manopla do Infinito" })).to.be.equal(true);
+    });
+  });
+
+  describe('#deleteProduct', () => {
+    it('deve retornar um erro ao tentar excluir um produto que não existe', async () => {
+      const req = {};
+      const res = {};
+
+      req.body = {};
+      req.params = { id: 99};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+
+      expect(productController.deleteProduct(req, res)).to.be.rejectedWith(NotFoundError);
+    });
+
+    it('deve retornar o produto excluído ao tentar excluir um produto que existe', async () => {
+      const req = {};
+      const res = {};
+
+      req.params = { id: 3 };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+
+      sinon.stub(productService, 'deleteProduct').resolves(productsMock[2]);
+
+      expect(productController.deleteProduct(req, res)).to.be.fulfilled;
+    })
   });
 })
