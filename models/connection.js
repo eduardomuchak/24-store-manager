e148cf180c7b94673987692ca12bdab1c55a73b8
Eduardo Muchak <eduardomuchak@gmail.com>
2022-07-06 11:25:44 +0000

    fix: tentativa de rodar avaliador corretamente

--- models/connection.js
+++ models/connection.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT || 3306,
   host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
